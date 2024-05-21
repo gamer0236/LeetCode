9e0f3d21fc0812fab4547132f63ce59d14b5fba3
gamer0236 <wwwabcdef577@gmail.com>
2024-05-21 12:22:15 +0530

    13th commit

---
+++ pandas for beginners/reshape data/pivot.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
+    return weather.pivot_table(index='month', columns='city', values='temperature', aggfunc='max')
+
+
+# The table is pivoted, each column represents a city, and each row represents a specific month.
+# The temperature values are aggregated using the max function.
+
+
+# sample Input:
+
+
+# +--------------+----------+-------------+
+# | city         | month    | temperature |
+# +--------------+----------+-------------+
+# | Jacksonville | January  | 13          |
+# | Jacksonville | February | 23          |
+# | Jacksonville | March    | 38          |
+# | Jacksonville | April    | 5           |
+# | Jacksonville | May      | 34          |
+# | ElPaso       | January  | 20          |
+# | ElPaso       | February | 6           |
+# | ElPaso       | March    | 26          |
+# | ElPaso       | April    | 2           |
+# | ElPaso       | May      | 43          |
+# +--------------+----------+-------------+
+
+
+# sample Output:
+# +----------+--------+--------------+
+# | month    | ElPaso | Jacksonville |
+# +----------+--------+--------------+
+# | April    | 2      | 5            |
+# | February | 6      | 23           |
+# | January  | 20     | 13           |
+# | March    | 26     | 38           |
+# | May      | 43     | 34           |
+# +----------+--------+--------------+
